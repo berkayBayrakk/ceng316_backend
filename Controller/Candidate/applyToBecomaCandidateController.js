0b6581f7043f5d747a6102d9d427722619f99c1a
Berkay Bayrak <berkay@Berkays-MacBook-Air.local>
2023-06-01 19:41:07 +0300

    apply become candidate controller updated

--- Controller/Candidate/applyToBecomaCandidateController.js
+++ Controller/Candidate/applyToBecomaCandidateController.js
@@ -1,17 +1,26 @@
 const {createCandidate}=require('../../Model/CandidateData');
 const {getStudentById}=require('../../Model/StudentData');
 const {getElectionById}=require('../../Model/ElectionData');
+const {createForm}=require('../../Model/FormData');
+
+
 
 
 async function applyToBecomeCandidate(req,res){
     const{studentId,electionId}=req.body;
+    const files = req.files;
     try {
+        if(!files) return res.status(400).json({'message':'No file uploaded.'});
+
         //check student's department id and election's department id are same
         const election=await getElectionById(electionId);
         const student=await getStudentById(studentId);
         if(election.department_id!==student.department_id)return res.status(400).json({'message':'Bad Request'});
 
         const candidate=await createCandidate(studentId,electionId);
+        files.forEach(async file => {
+            await createForm(candidate.id,file.buffer,file.originalname,file.mimetype,file.size);
+        });
         return res.json(candidate);
     } catch (error) {
         res.status(500).json({"message":`${error}`});
