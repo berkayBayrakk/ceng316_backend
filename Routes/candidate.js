ef2e0f4b347615f4f4d13909a65a4024b400629b
Berkay Bayrak <berkay@Berkays-MacBook-Air.local>
2023-06-01 19:40:17 +0300

    apply become candidate method place changed student to candidate root

--- Routes/candidate.js
+++ Routes/candidate.js
@@ -1,8 +1,12 @@
 const express=require('express');
-const electionRoute=express.Router();
+const candidateRoute=express.Router();
 
 const getCandidatesByElectionId=require('../Controller/Candidate/getCandidatesByElectionIdAndStatusController');
+const applyToBecomdeCandidate=require('../Controller/Candidate/applyToBecomaCandidateController');
 
+// Multer configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 /**
  * @swagger
@@ -37,6 +41,41 @@ const getCandidatesByElectionId=require('../Controller/Candidate/getCandidatesBy
  *       500:
  *         description: Server error
  */
-electionRoute.get('/', getCandidatesByElectionId);
+candidateRoute.get('/', getCandidatesByElectionId);
 
-module.exports=electionRoute;
+
+/**
+ * @swagger
+ *
+ * /student/apply-become-candidate:
+ *   post:
+ *     tags: 
+ *       - "Student"
+ *     summary: "Apply to become candidate"
+ *     description: Student applies to become candidate.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required: true
+ *         schema: 
+ *           type: object
+ *           properties:
+ *              studentId:
+ *                type: integer
+ *                example: 1
+ *              electionId:
+ *                type: integer
+ *                example: 1
+ *     responses:
+ *       201:
+ *         description: Candidate created.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Server error.
+ */
+candidateRoute.post('/apply-become-candidate',upload.array('pdf'),applyToBecomdeCandidate);
+
+module.exports=candidateRoute;
--- Routes/student.js
+++ Routes/student.js
@@ -1,39 +1,5 @@
-const applyToBecomdeCandidate=require('../Controller/Candidate/applyToBecomaCandidateController');
 const express=require('express');
 const studentRoute=express.Router();
-/**
- * @swagger
- *
- * /student/apply-become-candidate:
- *   post:
- *     tags: 
- *       - "Student"
- *     summary: "Apply to become candidate"
- *     description: Student applies to become candidate.
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: body
- *         in: body
- *         required: true
- *         schema: 
- *           type: object
- *           properties:
- *              studentId:
- *                type: integer
- *                example: 1
- *              electionId:
- *                type: integer
- *                example: 1
- *     responses:
- *       201:
- *         description: Candidate created.
- *       400:
- *         description: Bad request.
- *       500:
- *         description: Server error.
- */
-studentRoute.post('/apply-become-candidate',applyToBecomdeCandidate);
 
 
 module.exports=studentRoute;
