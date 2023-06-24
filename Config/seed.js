8365df061f2f126766439dbd156409b501a27d44
Berkay Bayrak <berkay@192.168.1.6>
2023-06-24 21:22:28 +0300

    database table updated

--- Config/seed.js
+++ Config/seed.js
@@ -1,6 +1,7 @@
 
 const bcrypt=require('bcrypt');
-const {sql}=require('./PostgreNeonConfig')
+const {sql}=require('./PostgreNeonConfig');
+
 async function createFacultyInstances(){
     await sql`INSERT INTO faculty (name) VALUES ('Faculty 1');`;
     await sql`INSERT INTO faculty (name) VALUES ('Faculty 2');`;
@@ -13,18 +14,15 @@ async function createDeaprtmentInstances(){
     //await sql`INSERT INTO Department (name,faculty_id) VALUES ('Department 3',2);`;
 }
 
-
 //berkay123
 //enis123
 //enes123
 async function createStudentInstances(){
-    const cryptedPassword=await bcrypt.hash("admin321",10);
-
-    await sql`INSERT INTO Student (name,password,email,department_id,role) VALUES ('Enes Dogan',${cryptedPassword}::text,'admin2@iyte.edu.tr',3,'admin');`;
+    const cryptedPassword=await bcrypt.hash("test123",10);
+    //INSTERT INTO Election (start_time,end_time,department_id) VALUES (2024-01-01,2025-01-01,10)
+    await sql`INSERT INTO Student (name,password,email,department_id,role) VALUES ('student',${cryptedPassword}::text,'student11@std.iyte.edu.tr',11,'student');`;
     //await sql`INSERT INTO Student (name,password,email,department_id) VALUES ('Student 2','${cryptedPassword}','student2@gmail.com',1);`;
     //await sql`INSERT INTO Student (name,password,email,department_id) VALUES ('Student 3','${cryptedPassword}','student3@gmail.com',2);`;
 }
 
-
 module.exports={createFacultyInstances,createDeaprtmentInstances,createStudentInstances};
-
--- scriptv1.sql
+++ scriptv1.sql
@@ -27,7 +27,7 @@ CREATE TABLE Student (
     name TEXT NOT NULL,
     email TEXT,
     department_id INTEGER REFERENCES Department(id),
-    role TEXT DEFAULT 'undergraduate',
+    role TEXT,
     tenure DATE
 );
 
@@ -37,21 +37,19 @@ CREATE TABLE Candidate (
     correction boolean DEFAULT false,
     status TEXT NOT NULL,
     student_id INTEGER REFERENCES Student(id) NOT NULL,
-    election_id INTEGER REFERENCES Election(id) NOT NULL,
+    election_id INTEGER REFERENCES Election(id) NOT NULL ON DELETE CASCADE,
     comment TEXT 
 );
 
 -- Form Table
 CREATE TABLE Form (
     id SERIAL PRIMARY KEY,
-    candidate_id INTEGER REFERENCES Candidate(id) NOT NULL,
+    candidate_id INTEGER REFERENCES Candidate(id) NOT NULL ON DELETE CASCADE,
     file_data BYTEA,
     file_name TEXT,
     mime_type TEXT,
     file_size INTEGER
 );
-
-
 
 -- Election Table
 CREATE TABLE Election (
@@ -65,22 +63,15 @@ CREATE TABLE Election (
 -- Election Result Table
 CREATE TABLE ElectionResult (
     id SERIAL PRIMARY KEY,
-    election_id INTEGER REFERENCES Election(id) NOT NULL,
-    winner_student_id INTEGER,
-    votes INTEGER
-);
-
--- Notification Table
-CREATE TABLE Notification (
-    id SERIAL PRIMARY KEY,
-    receiver_id INTEGER NOT NULL,
-    message TEXT NOT NULL
+    department_id INTEGER REFERENCES Department(id) NOT NULL UNIQUE,
+    winner_student_id INTEGER[],
+    vote_count INTEGER
 );
 
 -- Vote Table (Joint)
 CREATE TABLE Vote (
 	student_id INTEGER REFERENCES Student(id),
-	election_id INTEGER REFERENCES Election(id),
+	election_id INTEGER REFERENCES Election(id) ON DELETE CASCADE,
+    candidate_id INTEGER REFERENCES Candidate(id) ON DELETE CASCADE,
 	PRIMARY KEY(Student_id, election_id)
-
 );
